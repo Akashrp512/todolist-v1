911b2368a6643ebffd14282cc77dd4d81e0011c6
Akash RP <akashrp512@gmail.com>
2022-11-19 21:27:17 +0530

    commit

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
   
     var today = new Date();
     if(today.getDay() === 6 || today.getDay() === 0){
-        res.send("Yah it's the Weekend!")
+        res.send("<h1>Yah it's the Weekend!</h1>")
     }
     else{
-        res.send("Boo! I have to Work")
+        res.sendFile(__dirname+ '/index.html' )
     }
 });
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
+
+//use res.write to write multiple pieces of data coz res.send will only send one data
