5603d3f1263dc4fa614303af9361f897bfe0e956
Akash RP <akashrp512@gmail.com>
2022-12-01 14:56:06 +0530

    commit

--- app.js
+++ app.js
@@ -12,33 +12,34 @@ app.get("/", function(req, res){
     var today = new Date();
     var currentDay = today.getDay();
     var day = "";
+
    switch (currentDay) {
     case 0:
-        day: "Sunday"
+        day= "Sunday"
         break;
         case 1:
-            day: "Monday"
+            day= "Monday"
             break;
             case 2:
-                day: "Tuesday"
+                day= "Tuesday"
                 break;
                 case 3:
-                    day: "Wednesday"
+                    day= "Wednesday"
                     break;
                     case 4:
-                        day: "Thursday"
+                        day= "Thursday"
                         break;
                         case 5:
-                            day: "Friday"
+                            day= "Friday"
                             break;
                             case 6:
-                                day: "Saturday"
+                                day= "Saturday"
                                 break;
    
     default:
         console.log("Error current day is equal to:" +currentDay)
    }
-    res.render("list", {kindOfDay: 'day'});
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
