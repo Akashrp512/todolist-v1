1ffc4337da26122b20f696392b3ebeb7d7fcdbd0
Akash RP <akashrp512@gmail.com>
2022-12-01 17:57:30 +0530

    Styles added

--- app.js
+++ app.js
@@ -2,45 +2,34 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
+let items = ['buy food', 'cook food'] ;
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
   
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: 'numeric',
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
 
-   switch (currentDay) {
-    case 0:
-        day= "Sunday"
-        break;
-        case 1:
-            day= "Monday"
-            break;
-            case 2:
-                day= "Tuesday"
-                break;
-                case 3:
-                    day= "Wednesday"
-                    break;
-                    case 4:
-                        day= "Thursday"
-                        break;
-                        case 5:
-                            day= "Friday"
-                            break;
-                            case 6:
-                                day= "Saturday"
-                                break;
-   
-    default:
-        console.log("Error current day is equal to:" +currentDay)
-   }
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", (req,res)=>{
+  let  item = req.body.newItem;
+
+    items.push(item);
+  res.redirect("/");
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
