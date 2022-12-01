5e7de64d7786647fc76e3279bf5da1284265759f
Akash RP <akashrp512@gmail.com>
2022-12-01 19:47:15 +0530

    Version1 final commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ['buy food', 'cook food'] ;
+let WorkList = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,16 +21,31 @@ app.get("/", function(req, res){
     };
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", (req,res)=>{
   let  item = req.body.newItem;
-
+     if(req.body.list === 'Work'){
+       WorkList.push(item);
+       res.redirect("/work");
+     } else {
+       items.push(item);
+       res.redirect("/");
+     }
     items.push(item);
   res.redirect("/");
 });
 
+app.get("/work", (req,res)=>{
+   res.render("list",{listTitle: "Work List", newListItems:WorkList})
+});
+
+app.post("/work", (req,res)=>{
+  let item= req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
